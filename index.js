1745571d3b59b50ec4c53af781632f2af1a9f370
its_viru <viru03@git.com>
2024-06-07 16:48:17 +0530

    Implementation of episode-3

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child-1" }, [
-    React.createElement("h1", {}, "This is h1 tag"),
-    React.createElement("h2", {}, "This is h2 tag"),
-  ]),
-  React.createElement("div", { id: "child-2", className: "child-wrapper" }, [
-    React.createElement("h1", {}, "This is h1 tag"),
-    React.createElement("h2", {}, "This is h2 tag"),
-  ]),
-]);
+import AppComponent from "./components/app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log("🚀 ~ root:", root);
-root.render(parent);
+root.render(<AppComponent />);
