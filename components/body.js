1c065e340b21712d2e0a1750b2a7e6c7311c11c7
its_viru <viru03@git.com>
2024-06-07 16:48:17 +0530

    implementation of episode-4

---
+++ components/body.js
@@ -0,0 +1,11 @@
+import RestaurantComponent from "./restaurant,js";
+
+const BodyComponent = () => {
+  return (
+    <div className="body-wrapper">
+      <RestaurantComponent />
+    </div>
+  );
+};
+
+export default BodyComponent;
